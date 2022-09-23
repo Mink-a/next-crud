cf43b456680667bb16966f675c19438a80b854d1
Mink-a <rahulakhant@gmail.com>
2022-09-24 01:21:53 +0630

    cockroachdb connect

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,28 @@ const Home: NextPage<Notes> = ({ notes }) => {
         },
         method: "POST",
       }).then(() => {
-        setForm({ title: "", content: "", id: "" });
+        if(data.id){
+          deleteNote(data.id)
+          setForm({ title: "", content: "", id: "" });
+          refreshData();
+        }else{
+          setForm({ title: "", content: "", id: "" });
+          refreshData();
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function deleteNote(id: string) {
+    try {
+      fetch(`http://localhost:3000/api/note/${id}`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "DELETE",
+      }).then(() => {
         refreshData();
       });
     } catch (error) {
@@ -127,9 +148,63 @@ const Home: NextPage<Notes> = ({ notes }) => {
         <ul>
           {notes.map((note) => {
             return (
-              <li key={note.id} className="mt-4 border-b border-indigo-700">
-                <h3 className="font-bold">{note.title}</h3>
-                <p className="mb-1 text-sm">{note.content}</p>
+              <li
+                key={note.id}
+                className="group mt-4 border-b border-indigo-700"
+              >
+                <div className="flex items-center justify-between">
+                  <div className="">
+                    <h3 className="font-bold">{note.title}</h3>
+                    <p className="mb-1 text-sm">{note.content}</p>
+                  </div>
+                  <div className="flex gap-2">
+                    <div onClick={()=>{setForm({ title: note.title, content: note.content, id: note.id })}} className="hidden cursor-pointer group-hover:block">
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M21.2635 2.29289C20.873 1.90237 20.2398 1.90237 19.8493 2.29289L18.9769 3.16525C17.8618 2.63254 16.4857 2.82801 15.5621 3.75165L4.95549 14.3582L10.6123 20.0151L21.2189 9.4085C22.1426 8.48486 22.338 7.1088 21.8053 5.99367L22.6777 5.12132C23.0682 4.7308 23.0682 4.09763 22.6777 3.70711L21.2635 2.29289ZM16.9955 10.8035L10.6123 17.1867L7.78392 14.3582L14.1671 7.9751L16.9955 10.8035ZM18.8138 8.98525L19.8047 7.99429C20.1953 7.60376 20.1953 6.9706 19.8047 6.58007L18.3905 5.16586C18 4.77534 17.3668 4.77534 16.9763 5.16586L15.9853 6.15683L18.8138 8.98525Z"
+                          fill="#00f"
+                        />
+                        <path
+                          d="M2 22.9502L4.12171 15.1717L9.77817 20.8289L2 22.9502Z"
+                          fill="#00f"
+                        />
+                      </svg>
+                    </div>
+                    <div
+                      onClick={() => {
+                        deleteNote(note.id);
+                      }}
+                      className="hidden cursor-pointer group-hover:block"
+                    >
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M8 11C7.44772 11 7 11.4477 7 12C7 12.5523 7.44772 13 8 13H16C16.5523 13 17 12.5523 17 12C17 11.4477 16.5523 11 16 11H8Z"
+                          fill="#f00"
+                        />
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12ZM21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+                          fill="#f00"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
               </li>
             );
           })}
